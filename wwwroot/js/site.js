c78836a51767b190a8aefb9891d9c2e48e9e0224
MertJokjeck <ozogulmert7@gmail.com>
2023-04-12 23:28:46 +0300

    first commit

---
+++ wwwroot/js/site.js
@@ -0,0 +1,23 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+/*
+
+@foreach (var item in Model.lessons!)
+{
+    <div class="card-group">
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">@item.LessonName</h5>
+                <hr>
+                <p class="card-text"><b>Note:</b> @item.Note</p>
+                <p class="card-text"><b>Credit:</b> @item.Credit</p>
+                <a href="/delete-lesson/@item.Id" class="btn btn-danger">Delete</a>
+            </div>
+        </div>
+    </div>
+    <br>
+}
+*/
